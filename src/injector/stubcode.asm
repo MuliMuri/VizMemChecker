434a21bc9d9f413485cd2c35b792303fd3ce0fce
mulimuri <1282160815@qq.com>
2023-07-04 05:04:23 +0800

    update to rely on MemFrame StatckFrame and more

--- src/injector/stubcode.asm
+++ src/injector/stubcode.asm
@@ -7,7 +7,7 @@
 ; public HANDLER_PreCall
 ; public SaveRegsAndJmpBack
 
-; extern g_regs:DWORD
+; extern g_context:DWORD
 
 ; HANDLER_PreCall PROC FAR:
 ; 	;
@@ -17,35 +17,35 @@
 ; 	; push address of HANDLER_xxx
 ; 	;
 
-;     mov DWORD PTR [g_regs], eax
+;     mov DWORD PTR [g_context], eax
 
 ; 	; pop eax					; address of HANDLER_xxx
 ; 	; pop g_node
 
-; 	; mov g_regs.ecx, ecx
+; 	; mov g_context.ecx, ecx
 
 ; 	; mov ecx, ss:[esp]
-; 	; mov g_regs.ret, ecx		; address of caller
+; 	; mov g_context.ret, ecx		; address of caller
 
-; 	; mov g_regs.edx, edx
-; 	; mov g_regs.ebx, ebx
-; 	; mov g_regs.esp, esp		; now in here, the address is same to raw
-; 	; mov g_regs.ebp, ebp
-; 	; mov g_regs.esi, esi
-; 	; mov g_regs.edi, edi
+; 	; mov g_context.edx, edx
+; 	; mov g_context.ebx, ebx
+; 	; mov g_context.esp, esp		; now in here, the address is same to raw
+; 	; mov g_context.ebp, ebp
+; 	; mov g_context.esi, esi
+; 	; mov g_context.edi, edi
 
 ; 	; jmp eax
 ; HANDLER_PreCall ENDP
 
 ; SaveRegsAndJmpBack PROC:
-;     mov eax, g_regs.eax
-;     mov ecx, g_regs.ecx
-;     mov edx, g_regs.edx
-;     mov ebx, g_regs.ebx
-;     mov esp, g_regs.esp
-;     mov ebp, g_regs.ebp
-;     mov esi, g_regs.esi
-;     mov edi, g_regs.edi
+;     mov eax, g_context.eax
+;     mov ecx, g_context.ecx
+;     mov edx, g_context.edx
+;     mov ebx, g_context.ebx
+;     mov esp, g_context.esp
+;     mov ebp, g_context.ebp
+;     mov esi, g_context.esi
+;     mov edi, g_context.edi
 ;     jmp g_execBuffer
 	
 ; SaveRegsAndJmpBack ENDP
